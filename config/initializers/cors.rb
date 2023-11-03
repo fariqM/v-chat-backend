603945ff419d7c1b88b729609bef040e8b03fffd
fariqM <maulana.fariq30@gmail.com>
2023-11-03 11:14:54 +0700

    add url origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:8080", 'https://v-chat.lavienna.my.id'
+    origins "http://localhost:8080", 'https://v-chat.lavienna.my.id', "https://localhost", "http://localhost"
 
     resource "*",
       headers: :any,
